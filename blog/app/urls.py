73e003de3049e3ee3a4c8757c5d3e76d6b8bb11a
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-06-19 00:18:41 +0530

    Routers Added, URLs Done

--- blog/app/urls.py
+++ blog/app/urls.py
@@ -1,2 +1,12 @@
 from django.urls import path,include
-urlpatterns = []
+from rest_framework.routers import DefaultRouter
+from app.views import (
+                        BlogViewSet,
+                        ReviewViewSet,
+                        CategoryViewSet,
+                      )
+router = DefaultRouter()
+router.register(r'blogs',BlogViewSet,basename='blogs')
+router.register(r'reviews',ReviewViewSet,basename='reviews')
+router.register(r'categories',CategoryViewSet,basename='categories')
+urlpatterns = router.urls
