2516f95661aa109fe4c52ac53ddb9efc92f6517e
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-06-19 00:12:53 +0530

    Views File Written

--- blog/app/views.py
+++ blog/app/views.py
@@ -1,4 +1,30 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-class Blogs(viewsets.ModelViewSet):
-    pass
+from django_filters.rest_framework import DjangoFilterBackend
+from app.models import (
+                        Blog,
+                        Review,
+                        Category,
+                        )
+from app.serializers import (
+                            BlogSerializer,
+                            ReviewSerializer,
+                            CategorySerializer,
+                            )
+class BlogViewSet(viewsets.ModelViewSet):
+    queryset = Blog.objects.all()
+    serializer_class = BlogSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['blog_writer','blog_categories']
+    def perform_create(self, serializer):
+        serializer.save(blog_writer=self.request.user)
+class ReviewViewSet(viewsets.ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['review_writer']
+    def perform_create(self, serializer):
+        serializer.save(review_writer=self.request.user)
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
