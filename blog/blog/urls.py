182546d60af6552b0a1dce34d19c7f348f6b08b6
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-06-18 22:21:11 +0530

    App Level URLs Recognized By Project Level URLs Now

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
@@ -14,5 +14,6 @@ schema_view = get_schema_view(
 )
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('restapi/',include('app.urls')),
     path('',schema_view.with_ui('swagger'),name='schema-swagger-ui'),
 ]
