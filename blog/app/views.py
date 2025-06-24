b093906cf26b61651f782e8be17895bab6c3bde8
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-06-24 18:56:18 +0530

    Views File Basic Structre Defined

--- blog/app/views.py
+++ blog/app/views.py
@@ -1,1 +1,8 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+class BlogVS(ModelViewSet):
+    pass
+class ReviewVS(ModelViewSet):
+    pass
+class CategoryVS(ModelViewSet):
+    pass
