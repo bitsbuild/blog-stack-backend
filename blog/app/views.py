a471a6813be198fa906cf7b1510061d6c3ee4fdd
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-06-24 22:05:32 +0530

    Permisisons Class Added For Appropriate Viewsets

--- blog/app/views.py
+++ blog/app/views.py
@@ -4,15 +4,16 @@ from app.serializers import BlogSerializer,ReviewSerializer,CategorySerializer
 from app.models import Reviews,Category,Blog
 from rest_framework.throttling import UserRateThrottle
 from rest_framework.permissions import IsAuthenticated,IsAdminUser
+from app.permissions import AccessByOwnership
 class BlogVS(ModelViewSet):
     queryset = Blog.objects.all()
     serializer_class = BlogSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated,AccessByOwnership]
     throttle_classes = [UserRateThrottle]
 class ReviewVS(ModelViewSet):
     queryset = Reviews.objects.all()
     serializer_class = ReviewSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated,AccessByOwnership]
     throttle_classes = [UserRateThrottle]
 class CategoryVS(ModelViewSet):
     queryset = Category.objects.all()
