64759dd000a0a21a1d23c7b166ae6e64af289978
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-06-24 18:02:07 +0530

    Models File Structure Established, Category Model Completed, Admin File Completed

--- blog/app/admin.py
+++ blog/app/admin.py
@@ -1,1 +1,5 @@
 from django.contrib import admin
+from app.models import Category,Blog,Reviews
+admin.site.register(Category)
+admin.site.register(Blog)
+admin.site.register(Reviews)
--- blog/app/models.py
+++ blog/app/models.py
@@ -1,1 +1,11 @@
 from django.db import models
+import uuid
+class Category(models.Model):
+    category_id = models.UUIDField(default=uuid.uuid4,editable=False,primary_key=True)
+    category_name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.category_name
+class Blog(models.Model):
+    pass
+class Reviews(models.Model):
+    pass
