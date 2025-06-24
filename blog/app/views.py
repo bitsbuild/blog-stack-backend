64cb95c77ffde0e957f6198e61b713c111bcd95e
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-06-24 22:02:14 +0530

    Permisison File Basic Structure Defined

--- blog/app/views.py
+++ blog/app/views.py
@@ -3,14 +3,18 @@ from rest_framework.viewsets import ModelViewSet
 from app.serializers import BlogSerializer,ReviewSerializer,CategorySerializer
 from app.models import Reviews,Category,Blog
 from rest_framework.throttling import UserRateThrottle
+from rest_framework.permissions import IsAuthenticated,IsAdminUser
 class BlogVS(ModelViewSet):
     queryset = Blog.objects.all()
     serializer_class = BlogSerializer
+    permission_classes = [IsAuthenticated]
     throttle_classes = [UserRateThrottle]
 class ReviewVS(ModelViewSet):
     queryset = Reviews.objects.all()
     serializer_class = ReviewSerializer
+    permission_classes = [IsAuthenticated]
     throttle_classes = [UserRateThrottle]
 class CategoryVS(ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    permission_classes = [IsAdminUser]
