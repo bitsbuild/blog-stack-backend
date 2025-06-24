901e2c7639246e5c882ffb47f1e297290634c867
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-06-24 22:18:52 +0530

    Permisisons Written

--- blog/app/permissions.py
+++ blog/app/permissions.py
@@ -1,3 +1,10 @@
-from rest_framework.permissions import IsAuthenticated
-class AccessByOwnership(IsAuthenticated):
-    pass
+from rest_framework.permissions import BasePermission,SAFE_METHODS
+class AccessByOwnership(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            return True
+        else:
+            if request.user == obj.owner:
+                return True
+            else:
+                return False
