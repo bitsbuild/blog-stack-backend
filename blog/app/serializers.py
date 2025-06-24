93bff5d40b86f732ab9846b6d0497539095624a8
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-06-24 18:41:40 +0530

    Serializer File Structure Established

---
+++ blog/app/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework.serializers import ModelSerializer
+class BlogSerializer(ModelSerializer):
+    pass
+class ReviewSerializer(ModelSerializer):
+    pass
+class CategorySerializer(ModelSerializer):
+    pass
