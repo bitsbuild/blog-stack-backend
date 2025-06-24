8025397c85e05dd83576c3e11e88815de59d5ed4
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-06-24 21:31:13 +0530

    Endpoints Completed Logically, Authenticatio,Pagination,Throttling,Permissions,Pagination Pending

--- blog/app/serializers.py
+++ blog/app/serializers.py
@@ -1,12 +1,16 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer,SlugField,StringRelatedField
 from app.models import Category,Blog,Reviews
-class BlogSerializer(ModelSerializer):
-    class Meta:
-        model = Blog
-        fields = '__all__'
 class ReviewSerializer(ModelSerializer):
+    review_writer = StringRelatedField()
+    for_blog = StringRelatedField()
     class Meta:
         model = Reviews
+        fields = '__all__'
+class BlogSerializer(ModelSerializer):
+    blog_categories = StringRelatedField(read_only=True,many=True)
+    reviews = ReviewSerializer(many=True)
+    class Meta:
+        model = Blog
         fields = '__all__'
 class CategorySerializer(ModelSerializer):
     class Meta:
