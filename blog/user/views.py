eb30cc9e64ac1777839f1ca3913f44b506e1a050
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-06-24 14:51:37 +0530

    Views Written With Appropriate Permissions Set

--- blog/user/views.py
+++ blog/user/views.py
@@ -1,7 +1,40 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import status
 from rest_framework.decorators import api_view,permission_classes
 from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth.models import User
+from user.serializers import UserSerializer
+@api_view(['POST'])
 def create_user(request):
-    pass
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(
+            {
+            "Status":"Account Creation Successful"
+            },
+            status=status.HTTP_200_OK,
+        )
+    else:
+        return Response(
+            {
+            "Status":"Account Creation Failed"
+            },
+            status=status.HTTP_400_BAD_REQUEST,
+        )
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
 def delete_user(request):
-    pass
+    try:
+        request.user.delete()
+        return Response(
+            {
+                "Status":"Account Deletion Successful"
+            },
+            status=status.HTTP_200_OK
+        )
+    except Exception as e:
+        return Response({
+            "Status":"Account Deletion Failed",
+            "Error":str(e)
+        },status=status.HTTP_400_BAD_REQUEST)
