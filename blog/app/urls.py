ed76bddfb0de3c10f10b1f7b5b643ee8c9075d35
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-06-24 20:28:17 +0530

    URLs Defined For Blog App

--- blog/app/urls.py
+++ blog/app/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path,include
-urlpatterns = [
-    
-]
+from rest_framework.routers import BaseRouter
+from app.views import BlogVS,ReviewVS,CategoryVS
+router = BaseRouter()
+router.register(r'blogs',BlogVS,basename='blogs')
+router.register(r'review',ReviewVS,basename='review')
+router.register(r'category',CategoryVS,basename='category')
+urlpatterns = router.urls
