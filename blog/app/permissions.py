9cd21d7d69479e7d576cfcb3925e3297781bb638
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-06-24 23:26:13 +0530

    Permissions Fixed And Filtering Added

--- blog/app/permissions.py
+++ blog/app/permissions.py
@@ -1,10 +1,19 @@
 from rest_framework.permissions import BasePermission,SAFE_METHODS
-class AccessByOwnership(BasePermission):
+class BlogAccessByOwnership(BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in SAFE_METHODS:
             return True
         else:
             if request.user == obj.blog_writer:
+                return True
+            else:
+                return False
+class ReviewAccessByOwnership(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            return True
+        else:
+            if request.user == obj.review_writer:
                 return True
             else:
                 return False
--- blog/app/views.py
+++ blog/app/views.py
@@ -4,17 +4,22 @@ from app.serializers import BlogSerializer,ReviewSerializer,CategorySerializer
 from app.models import Reviews,Category,Blog
 from rest_framework.throttling import UserRateThrottle
 from rest_framework.permissions import IsAuthenticated,IsAdminUser
-from app.permissions import AccessByOwnership
+from app.permissions import BlogAccessByOwnership,ReviewAccessByOwnership
+from django_filters.rest_framework import DjangoFilterBackend
 class BlogVS(ModelViewSet):
     queryset = Blog.objects.all()
     serializer_class = BlogSerializer
-    permission_classes = [IsAuthenticated,AccessByOwnership]
+    permission_classes = [IsAuthenticated,BlogAccessByOwnership]
     throttle_classes = [UserRateThrottle]
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['blog_writer','blog_categories']
 class ReviewVS(ModelViewSet):
     queryset = Reviews.objects.all()
     serializer_class = ReviewSerializer
-    permission_classes = [IsAuthenticated,AccessByOwnership]
+    permission_classes = [IsAuthenticated,ReviewAccessByOwnership]
     throttle_classes = [UserRateThrottle]
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['for_blog','review_writer']
 class CategoryVS(ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
