d307a34935947ffb5ba54a41f02a10ae64cf19a2
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-06-24 14:10:20 +0530

    User Serializer Written

--- blog/user/serializers.py
+++ blog/user/serializers.py
@@ -1,4 +1,23 @@
 from django.contrib.auth.models import User
 from rest_framework.serializers import ModelSerializer,CharField,ValidationError
 class UserSerializer(ModelSerializer):
-    pass
+    confirm_password = CharField(write_only=True)
+    model = User
+    fields = ['username','password','email','confirm_password']
+    extra_kwargs = {
+        "password":{'write_only':True},
+    }
+    def validate(self,data):
+        if data['password'] == data['confirm_password']:
+            return data
+        else:
+            raise ValidationError(
+                {
+                    "Error":"Passwords Not Matching"
+                }
+            )
+    def create(self,validated_data):
+        user = User(username=validated_data['username'],email=validated_data['email'])
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
