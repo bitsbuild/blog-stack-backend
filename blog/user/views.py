b3cf833a2d12d8b8a42892eb800122fd87a7d12f
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-06-24 14:25:33 +0530

    Views File Structure Defined

--- blog/user/views.py
+++ blog/user/views.py
@@ -1,1 +1,7 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view,permission_classes
+from rest_framework.permissions import IsAuthenticated
+def create_user(request):
+    pass
+def delete_user(request):
+    pass
