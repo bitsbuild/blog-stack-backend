e1e644a3ad6169c3f34a66ee4297e9cecf4a7230
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-06-24 14:56:44 +0530

    User URL Endpoints Defined

--- blog/user/urls.py
+++ blog/user/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
+from user.views import create_user,delete_user
+from rest_framework.authtoken.views import obtain_auth_token
 ulrpatterns = [
-    
+    path('create/',create_user,name='create'),
+    path('token/',obtain_auth_token,name='token'),
+    path('delete/',delete_user,name='delete'),
 ]
