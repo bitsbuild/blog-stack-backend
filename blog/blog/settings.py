e355d2d3bf015b9ed499f9106195b0616a0a7e30
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-06-24 17:48:07 +0530

    Unit Testing Completed For User App, User App Development Complete

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -11,7 +11,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'user',
+    'user.apps.UserConfig',
     'rest_framework.authtoken',
 ]
 MIDDLEWARE = [
--- blog/user/tests.py
+++ blog/user/tests.py
@@ -1,15 +1,11 @@
 from rest_framework.test import APITestCase
 from rest_framework import status
 from django.urls import reverse
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import User
 class UserTesting(APITestCase):
     def setUp(self):
-        data = {
-            "username":"1jash_upadhyay1",
-            "password":"1123451",
-            "confirm_password":"1123451",
-            "email":"1test@xyz.com1"
-        }
-        self.client.post(reverse('create'),data)
+        self.user = User.objects.create_user(username="1jashupadhyay1",password="1123451",email="1jashupadhyay.java@gmail.com1")
     def test_create_user_success(self):
         data = {
             "username":"jash_upadhyay",
@@ -46,7 +42,13 @@ class UserTesting(APITestCase):
         }
         response = self.client.post(reverse('create'),data)
         self.assertEqual(response.status_code,status.HTTP_400_BAD_REQUEST)
-    def delete_user_success(self):
-        pass
-    def delete_user_failure(self):
-        pass
+    def test_delete_user_success(self):
+        token = Token.objects.get(user__username='1jashupadhyay1')
+        self.client.credentials(HTTP_AUTHORIZATION='Token '+str(token.key))
+        response = self.client.post(reverse('delete'))
+        self.assertEqual(response.status_code,status.HTTP_200_OK)
+    def test_delete_user_failure(self):
+        token = ''
+        self.client.credentials(HTTP_AUTHORIZATION='Token '+token)
+        response = self.client.post(reverse('delete'))
+        self.assertEqual(response.status_code,status.HTTP_401_UNAUTHORIZED)
