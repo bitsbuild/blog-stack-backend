ec47f1c9f3d257d310c740cc5c6ed184a9687e89
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-06-24 21:07:00 +0530

    Small Logical Error For Blog-Reviews Linking

--- blog/app/models.py
+++ blog/app/models.py
@@ -6,23 +6,23 @@ class Category(models.Model):
     category_name = models.CharField(max_length=150)
     def __str__(self):
         return self.category_name
-class Reviews(models.Model):
-    review_id = models.UUIDField(default=uuid.uuid4,editable=False,primary_key=True)
-    review_writer = models.ForeignKey(User,on_delete=models.CASCADE,editable=False)
-    review_title = models.CharField(max_length=150)
-    review_body = models.CharField(max_length=1500)
-    review_created = models.DateTimeField(auto_now_add=True,editable=False)
-    review_updated = models.DateTimeField(auto_now=True,editable=False)
-    def __str__(self):
-        return self.review_title
 class Blog(models.Model):
     blog_id = models.UUIDField(default=uuid.uuid4,editable=False,primary_key=True)
-    blog_writer = models.ForeignKey(User,on_delete=models.CASCADE,editable=False)
+    blog_writer = models.ForeignKey(User,on_delete=models.CASCADE)
     blog_title = models.CharField(max_length=150)
     blog_body = models.CharField(max_length=7500)
     blog_categories = models.ManyToManyField(Category,related_name='blogs')
-    blog_reviews = models.ManyToManyField(Reviews,related_name='blogs')
     blog_created = models.DateTimeField(auto_now_add=True,editable=False)
     blog_updated = models.DateTimeField(auto_now=True,editable=False)
     def __str__(self):
         return self.blog_title
+class Reviews(models.Model):
+    review_id = models.UUIDField(default=uuid.uuid4,editable=False,primary_key=True)
+    review_writer = models.ForeignKey(User,on_delete=models.CASCADE)
+    review_title = models.CharField(max_length=150)
+    review_body = models.CharField(max_length=1500)
+    review_created = models.DateTimeField(auto_now_add=True,editable=False)
+    review_updated = models.DateTimeField(auto_now=True,editable=False)
+    for_blog = models.ForeignKey(Blog,related_name='reviews',on_delete=models.CASCADE)
+    def __str__(self):
+        return self.review_title
--- blog/app/urls.py
+++ blog/app/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path,include
-from rest_framework.routers import BaseRouter
+from rest_framework.routers import DefaultRouter
 from app.views import BlogVS,ReviewVS,CategoryVS
-router = BaseRouter()
+router = DefaultRouter()
 router.register(r'blogs',BlogVS,basename='blogs')
 router.register(r'review',ReviewVS,basename='review')
 router.register(r'category',CategoryVS,basename='category')
