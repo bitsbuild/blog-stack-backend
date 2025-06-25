aff04691e7975b2b3354ce9b4a4933cee580af1a
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-06-25 15:28:38 +0530

    Blog Project Completed

--- blog/app/models.py
+++ blog/app/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 import uuid
 from django.contrib.auth.models import User
+
 class Category(models.Model):
     category_id = models.UUIDField(default=uuid.uuid4,editable=False,primary_key=True)
     category_name = models.CharField(max_length=150)
     def __str__(self):
         return self.category_name
+    
 class Blog(models.Model):
     blog_id = models.UUIDField(default=uuid.uuid4,editable=False,primary_key=True)
     blog_writer = models.ForeignKey(User,on_delete=models.CASCADE)
@@ -16,6 +18,7 @@ class Blog(models.Model):
     updated = models.DateTimeField(auto_now=True,editable=False)
     def __str__(self):
         return self.blog_title
+    
 class Reviews(models.Model):
     review_id = models.UUIDField(default=uuid.uuid4,editable=False,primary_key=True)
     review_writer = models.ForeignKey(User,on_delete=models.CASCADE)
